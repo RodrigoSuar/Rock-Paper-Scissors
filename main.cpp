0e2414c9a9977f46f552c7ba30ff32de02daf1a8
RodrigoSuar <suarez.rodrigo3004@gmail.com>
2023-07-28 03:23:08 -0500

    added main fuck with simple commands

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main () {
+    int choice;
+    std:: cout << "1.Rock\n2.Paper\n3.Scissor\nEnter a number";
+    std:: cin >> choice;
+    
+    
+    return 0;
+
+}
+
+bool outcome() {
+
+}
